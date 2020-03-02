96118c6a0edf45d624ccf2a9cca268e58ed9a813
enos2328 <aenosara@csumb.edu>
2020-03-01 20:38:09 -0800

    Initial Project Base

---
+++ Parstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parstagram
+//
+//  Created by Athena Enosara on 3/1/20.
+//  Copyright © 2020 Athena Enosara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
